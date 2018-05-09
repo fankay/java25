7110926138fe0d89e7246a669dc0678762126415
fankay <fankai@kaishengit.com>
2018-05-09 10:24:50 +0800

    演示SpringBoot的入门示例

---
+++ springboot-demo/src/main/java/com/kaishengit/Application.java
@@ -0,0 +1,16 @@
+package com.kaishengit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot项目的入口-启动类
+ * @author fankay
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
