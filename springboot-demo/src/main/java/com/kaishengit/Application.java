3bbb72ef68ccec9969a8937ad9d5e5b0c2dc2919
fankay <fankai@kaishengit.com>
2018-05-11 15:46:24 +0800

    演示Spring Boot 中的定时任务和异步任务

--- springboot-demo/src/main/java/com/kaishengit/Application.java
+++ springboot-demo/src/main/java/com/kaishengit/Application.java
@@ -2,12 +2,15 @@ package com.kaishengit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * SpringBoot项目的入口-启动类
  * @author fankay
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
---
+++ springboot-demo/src/main/java/com/kaishengit/job/SpringScheduJob.java
@@ -0,0 +1,16 @@
+package com.kaishengit.job;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringScheduJob {
+
+    @Scheduled(fixedRate = 3000)
+    @Async("taskExecutor")
+    public void fixedRateJob() {
+        System.out.println("fixedRateJob :" + System.currentTimeMillis());
+    }
+
+}
