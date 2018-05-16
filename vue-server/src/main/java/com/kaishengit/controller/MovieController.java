61396972c4ddf31cc3a4dae2d3623bf772d52372
fankay <fankai@kaishengit.com>
2018-05-16 16:30:37 +0800

    vue-router、axios、vue-cli的使用

--- vue-server/src/main/java/com/kaishengit/controller/MovieController.java
+++ vue-server/src/main/java/com/kaishengit/controller/MovieController.java
@@ -1,4 +1,27 @@
 package com.kaishengit.controller;
 
+import org.omg.PortableInterceptor.INACTIVE;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/movie")
+@CrossOrigin("*")
 public class MovieController {
+
+    @GetMapping("/{id}")
+    public Map<String,String> getMovie(@PathVariable Integer id) {
+        Map<String,String> map = new HashMap<>();
+        map.put("name","阿甘正传");
+        map.put("rate","9.5");
+        map.put("author","Jack");
+        return map;
+    }
+
 }
