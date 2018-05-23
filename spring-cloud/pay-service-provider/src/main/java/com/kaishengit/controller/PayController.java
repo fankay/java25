5996529f75bb9717b158b19450fc99ef5085c204
fankay <fankai@kaishengit.com>
2018-05-23 11:44:07 +0800

    Spring cloud config的使用

--- spring-cloud/pay-service-provider/src/main/java/com/kaishengit/controller/PayController.java
+++ spring-cloud/pay-service-provider/src/main/java/com/kaishengit/controller/PayController.java
@@ -1,20 +1,28 @@
 package com.kaishengit.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pay")
+@RefreshScope
 public class PayController {
+
+    @Value("${tax}")
+    private double tax;
 
     @GetMapping("/alipay")
     public String alipay(String payAccount,double money) {
-        return payAccount + " alipay success :" + money;
+        double payTax = money * tax;
+        return payAccount + " alipay success :" + money + "  tax:" + payTax;
     }
 
     @GetMapping("/weixin")
     public String weixin(String payAccount,double money) {
-        return payAccount + " weixin pay success :" + money;
+        double payTax = money * tax;
+        return payAccount + " weixin pay success :" + money + "  tax:" + payTax;
     }
 }
